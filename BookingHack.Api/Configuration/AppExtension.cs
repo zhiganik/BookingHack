cb367f4b75fd3f2f08b3e61c4ee52cb223e7fe3c
zh1ganik <mykyta.zhehistovskyi@stepico.com>
2026-05-11 18:29:26 +0200

    Add exception handler middleware. Some logs polishing

--- BookingHack.Api/Configuration/AppExtension.cs
+++ BookingHack.Api/Configuration/AppExtension.cs
@@ -7,7 +7,14 @@ public static class AppExtension
     public static IApplicationBuilder Configure(this WebApplication app)
     {
         app.UseSerilogRequestLogging();
-        
+
+        app.UseExceptionHandler(err => err.Run(async ctx =>
+        {
+            ctx.Response.StatusCode = 500;
+            ctx.Response.ContentType = "application/json";
+            await ctx.Response.WriteAsJsonAsync(new { message = "An unexpected error occurred." });
+        }));
+
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
